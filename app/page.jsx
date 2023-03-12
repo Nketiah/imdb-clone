32c293434b10af43c464331e2b22365d0a622e4d
Nketiah Joseph <gyatabajoe@gmail.com>
2023-03-12 09:47:18 +0000

    install next js

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+
+  // responsible for loading index/home page
+
+  return (
+    <main>
+      <h1>Home</h1>
+    </main>
+  )
+}
